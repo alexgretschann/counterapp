3f123aff2c679ef143a4221dcb01a29da74519c9
Alex Gretschann <alexander.gretschann@mni.thm.de>
2015-02-22 17:07:57 +0100

    Add cupertino style

--- app/view/Main.js
+++ app/view/Main.js
@@ -10,7 +10,7 @@ Ext.define('counter.view.Main', {
 
         items: [
             {
-                title: 'Welcome',
+                title: 'Counter',
                 iconCls: 'home',
 
                 styleHtmlContent: true,
